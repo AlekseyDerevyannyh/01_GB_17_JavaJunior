39172cebb918c93a343828af170b3284af0da60f
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-11-23 18:43:02 +0300

    init Seminar 1

---
+++ seminar/01/lesson/src/main/java/ru/gb/task01/Task01Main.java
@@ -0,0 +1,19 @@
+package ru.gb.task01;
+
+public class Task01Main {
+    public static void main(String[] args) {
+
+    }
+
+    static class MyClass implements MyInterface {
+
+        @Override
+        public void MyMethod() {
+
+        }
+    }
+
+    interface MyInterface {
+        void MyMethod();
+    }
+}
