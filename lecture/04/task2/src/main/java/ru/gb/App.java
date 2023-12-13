100e8f505527a8a162102376f27ac0ab5215e159
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-12-13 13:27:23 +0300

    complete Lecture 4

--- lecture/04/task2/src/main/java/ru/gb/App.java
+++ lecture/04/task2/src/main/java/ru/gb/App.java
@@ -2,6 +2,9 @@ package ru.gb;
 
 public class App {
     public static void main(String[] args) {
-        Db.con();
+//        Db.toDb();
+//        Db.fromDb();
+//        Db.editDb();
+        Db.deleteFromDb();
     }
 }
--- lecture/04/task2/src/main/java/ru/gb/Db.java
+++ lecture/04/task2/src/main/java/ru/gb/Db.java
@@ -1,7 +1,10 @@
 package ru.gb;
 
-import java.sql.*;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import java.util.List;
 
 public class Db {
     private static final String URL = "jdbc:mysql://172.16.1.122:3306";
@@ -9,24 +12,71 @@ public class Db {
     private static final String PASSWORD = "admin";
 
 
-    public static void con() {
-        try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
-            Statement statement = con.createStatement();
-            statement.execute("DROP SCHEMA `test`");
-            statement.execute("CREATE SCHEMA `test`");
-            statement.execute("CREATE TABLE `test`.`table` (`id` INT NOT NULL, `firstname` VARCHAR(45) NULL, `lastname` VARCHAR(45) NULL, PRIMARY KEY(`id`));");
-            statement.execute("INSERT INTO `test`.`table` (`id`, `firstname`, `lastname`)\n" +
-                    "VALUES (1, 'Иванов', 'Иван');");
-            statement.execute("INSERT INTO `test`.`table` (`id`, `firstname`, `lastname`)\n" +
-                    "VALUES (2, 'Петров', 'Пётр');");
-            ResultSet set = statement.executeQuery("SELECT * FROM `test`.`table`;");
-            while (set.next()) {
-                System.out.println(set.getString(3) + " " + set.getString(2) + " " + set.getInt(1));
-            }
+    public static void toDb() {
+        Connector connector = new Connector();
+        Session session = connector.getSession();
 
-        } catch(SQLException e) {
-            throw new RuntimeException();
+        Magic magic = new Magic("Волшебная стрела", 10, 0, 0);
+
+        session.beginTransaction();
+
+        session.save(magic);
+        magic = new Magic("Молния", 25, 0, 0);
+        session.save(magic);
+        magic = new Magic("Каменная кожа", 0, 0, 6);
+        session.save(magic);
+        magic = new Magic("Жажда крови", 0, 6, 0);
+        session.save(magic);
+        magic = new Magic("Жажда крови", 0, 6, 0);
+        session.save(magic);
+        magic = new Magic("Проклятие", 0, -3, 0);
+        session.save(magic);
+        magic = new Magic("Лечение", 30, 0, 0);
+        session.save(magic);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public static void fromDb() {
+        Connector connector = new Connector();
+        try (Session session = connector.getSession()) {
+            List<Magic> books = session.createQuery("FROM Magic ", Magic.class).getResultList();
+            books.forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 
+    public static void editDb() {
+        Connector connector = new Connector();
+        try (Session session = connector.getSession()) {
+            String hql = "from Magic where id = :id";
+            Query<Magic> query = session.createQuery(hql, Magic.class);
+            query.setParameter("id", 4);
+            Magic magic = query.getSingleResult();
+            System.out.println(magic);
+            magic.setAttBonus(100);
+            magic.setName("Ярость");
+            session.beginTransaction();
+            session.update(magic);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteFromDb() {
+        Connector connector = new Connector();
+        try (Session session = connector.getSession()) {
+            Transaction t = session.beginTransaction();
+            List<Magic> magics = session.createQuery("FROM Magic ", Magic.class).getResultList();
+            magics.forEach(magic -> {
+                session.delete(magic);
+            });
+            t.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
