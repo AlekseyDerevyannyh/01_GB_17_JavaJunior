c898a71d19e7404880744bbed7dde3c759bdbf20
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-12-16 17:34:35 +0300

    init HW 4

--- seminar/04/lesson/src/main/java/ru/gb/lesson4/Jpa.java
+++ seminar/04/lesson/src/main/java/ru/gb/lesson4/Jpa.java
@@ -11,49 +11,49 @@ import java.util.stream.LongStream;
 
 public class Jpa {
 
-  public static void main(String[] args) throws SQLException {
-    // JPA Java Persistence API - набор правил (соглашений) по реализации доменной модели
-    // Entity
+    public static void main(String[] args) throws SQLException {
+        // JPA Java Persistence API - набор правил (соглашений) по реализации доменной модели
+        // Entity
 
-    // Hibernate -> одна из реализаций стандарта JPA
-    // EclipseLink
+        // Hibernate -> одна из реализаций стандарта JPA
+        // EclipseLink
 
-    final SessionFactory sessionFactory = new Configuration()
-      .configure("hibernate.cfg.xml").buildSessionFactory();
+        final SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml").buildSessionFactory();
 
-    try (Session session = sessionFactory.openSession()) {
-      session.beginTransaction();
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
 
-      List<User> users = LongStream.rangeClosed(1, 10)
-        .mapToObj(it -> new User(it))
-        .peek(it -> it.setName("User #" + it.getId()))
-        .peek(it -> session.persist(it))
-        .collect(Collectors.toList());
+            List<User> users = LongStream.rangeClosed(1, 10)
+                    .mapToObj(it -> new User(it))
+                    .peek(it -> it.setName("User #" + it.getId()))
+                    .peek(it -> session.persist(it))
+                    .collect(Collectors.toList());
 
-      session.getTransaction().commit();
-    }
+            session.getTransaction().commit();
+        }
 
-    final User loadedUser;
-    try (Session session = sessionFactory.openSession()) {
-      session.beginTransaction();
+        final User loadedUser;
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
 
-      loadedUser = session.get(User.class, 1);
-      loadedUser.setName("Updated");
+            loadedUser = session.get(User.class, 1);
+            loadedUser.setName("Updated");
 
 //      session.merge(loadedUser);
-      session.getTransaction().commit();
+            session.getTransaction().commit();
+        }
+
+        try (Session session = sessionFactory.openSession()) {
+            // jql java query language
+            // select id, name from users where id >= 1
+            List<User> users = session.createQuery("select u from User u where id >= 1 order by id desc", User.class)
+                    .getResultList();
+
+            System.out.println(users);
+        }
+
+        sessionFactory.close();
     }
-
-    try (Session session = sessionFactory.openSession()) {
-      // jql java query language
-      // select id, name from users where id >= 1
-      List<User> users = session.createQuery("select u from User u where id >= 1 order by id desc", User.class)
-        .getResultList();
-
-      System.out.println(users);
-    }
-
-    sessionFactory.close();
-  }
 
 }
--- seminar/04/lesson/src/main/java/ru/gb/lesson4/User.java
+++ seminar/04/lesson/src/main/java/ru/gb/lesson4/User.java
@@ -9,42 +9,42 @@ import jakarta.persistence.*;
 // у каждой сущности (entity) есть первичный ключ
 public class User {
 
-  public User() {
+    public User() {
 
-  }
+    }
 
-  public User(Long id) {
-    this.id = id;
-  }
+    public User(Long id) {
+        this.id = id;
+    }
 
-  @Id
+    @Id
 //  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    private Long id;
 
-  @Column(name = "name")
-  private String name;
+    @Column(name = "name")
+    private String name;
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  @Override
-  public String toString() {
-    return "User{" +
-      "id=" + id +
-      ", name='" + name + '\'' +
-      '}';
-  }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
