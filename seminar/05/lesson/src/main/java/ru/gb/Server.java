216c30a6179dc3625a71e57dec88b056de3f9997
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-12-21 22:33:56 +0300

    add Seminar 5

--- seminar/05/lesson/src/main/java/ru/gb/Server.java
+++ seminar/05/lesson/src/main/java/ru/gb/Server.java
@@ -1,34 +1,87 @@
 package ru.gb;
 
+import lombok.Getter;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Server {
+
+    // message broker (kafka, redis, rabbitmq, ...)
+    // client sent letter to broker
+
+    // server sent to SMTP-server
+
+    public static final int PORT = 8181;
+
+    private static long clientIdCounter = 1L;
+    private static Map<Long, SocketWrapper> clients = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(8181)) {
-            Socket client = server.accept();
-            System.out.println("Подключился новый клиент: " + client);
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            System.out.println("Сервер запущен на порту " + PORT);
+            while (true) {
+                final Socket client = server.accept();
+                final long clientId = clientIdCounter++;
 
-            OutputStream outputStream = client.getOutputStream();
-            new PrintWriter(outputStream);
-            try (Scanner input = new Scanner(client.getInputStream());
-                PrintWriter output = new PrintWriter(client.getOutputStream())) {
+                SocketWrapper wrapper = new SocketWrapper(clientId, client);
+                System.out.println("Подключился новый клиент[" + wrapper + "]");
+                clients.put(clientId, wrapper);
 
-                output.println("Подключение произошло успешно");
+                new Thread(() -> {
+                    try (Scanner input = wrapper.getInput(); PrintWriter output = wrapper.getOutput()) {
+                        output.println("Подключение успешно. Список всех клиентов: " + clients);
 
-                while (true) {
-                    String clientInputLine = input.nextLine();
-                    System.out.println("Сообщение от клиента" + clientInputLine);
+                        while (true) {
+                            String clientInput = input.nextLine();
+                            if (Objects.equals("q", clientInput)) {
+                                // todo разослать это сообщение всем остальным клиентам
+                                clients.remove(clientId);
+                                clients.values().forEach(it -> it.getOutput().println("Клиент[" + clientId + "] отключился"));
+                                break;
+                            }
 
-                    output.println("Сообщение [" + clientInputLine + "] получено");
-                }
+                            // формат сообщения: "цифра сообщение"
+                            long destinationId = Long.parseLong(clientInput.substring(0, 1));
+                            SocketWrapper destination = clients.get(destinationId);
+                            destination.getOutput().println(clientInput);
+                        }
+                    }
+                }).start();
             }
-
         }
+    }
+
+}
+
+@Getter
+class SocketWrapper implements AutoCloseable {
+
+    private final long id;
+    private final Socket socket;
+    private final Scanner input;
+    private final PrintWriter output;
+
+    SocketWrapper(long id, Socket socket) throws IOException {
+        this.id = id;
+        this.socket = socket;
+        this.input = new Scanner(socket.getInputStream());
+        this.output = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    @Override
+    public void close() throws Exception {
+        socket.close();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", socket.getInetAddress().toString());
     }
 }
